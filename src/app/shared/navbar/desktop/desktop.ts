9515faa103e57088fca64e5a93fbfcf10295735a
Rodolphe33 <rodolpheseul@gmail.com>
2026-09-11 18:35:01 +0200

    feat: modif de la navbar desktop

--- src/app/shared/navbar/desktop/desktop.ts
+++ src/app/shared/navbar/desktop/desktop.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
-import { Menubar } from 'primeng/menubar';
 import { Avatar } from 'primeng/avatar';
 import { ButtonDirective } from 'primeng/button';
 import { StyleClass } from 'primeng/styleclass';
 
 @Component({
-  imports: [Menubar, NgOptimizedImage, Avatar, ButtonDirective, StyleClass],
+  imports: [NgOptimizedImage, Avatar, ButtonDirective, StyleClass],
   selector: 'star-desktop',
-  styleUrl: './desktop.css',
+  styleUrl: './desktop.scss',
   templateUrl: './desktop.html',
 })
 export class Desktop {}
